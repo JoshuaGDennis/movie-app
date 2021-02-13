b5dc8de4587030b67385f09fcf42b284e956856e
JoshDennis <joshuagardennis@gmail.com>
2021-02-13 19:57:08 +0000

    setting up basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from "react";
-import styles from "./App.module.scss";
-import moduleClasses from "./helpers/moduleClasses";
+import MainPage from "./pages/MainPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const classes = moduleClasses(styles);
-
-function App() {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <header>
-        <p className={classes("text")}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Router>
+    <Route path="/" component={MainPage} />
+  </Router>
+);
 
 export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainPage = () => (
+  <div>
+    <h1>THIS IS THE MAIN PAGE</h1>
+  </div>
+);
+
+export default MainPage;
